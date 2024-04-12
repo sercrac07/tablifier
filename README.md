f7420015204531bbe5f8f625955b88d6366a6fea
Sercrac07 <sercassan@gmail.com>
2024-04-12 20:22:11 +0200

    Added description to Table class

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ Even More Console methods.
 ```bash
 npm install emcm
 ```
+
+## Usage
+
+### Table
+
+You can get a table by using the `Table` class.
+
+```javascript
+const table = new Table()
+table.addRow('This is the key', 'This is the value')
+
+console.log(table.toString())
+// output:
+// ┌────────────────┬──────────────────┐
+// │key             │value             │
+// ├────────────────┼──────────────────┤
+// │This is the key │This is the value │
+// └────────────────┴──────────────────┘
+```
--- src/table.ts
+++ src/table.ts
@@ -1,21 +1,59 @@
 import { getLargestLength } from '&/lib/strings'
-import { Row } from './types'
+import { Row } from '&/types'
 
+/**
+ * Create a new table.
+ *
+ * ```javascript
+ * const table = new Table()
+ * table.addRow('This is the key', 'This is the value')
+ * ```
+ */
 export class Table {
   key: string = 'key'
   value: string = 'value'
   rows: Row[] = []
 
+  /**
+   * Create the table. You can pass the key and value title as parameters.
+   *
+   * ```
+   * const table = new Table('Key title', 'Value title')
+   * ```
+   */
   constructor(key?: string, value?: string) {
     this.key = key ?? 'key'
     this.value = value ?? 'value'
   }
 
+  /**
+   * Adds a row to the table.
+   *
+   * ```javascript
+   * table.addRow('First key', 'First value')
+   * table.addRow('Second key', 'Second value')
+   * ```
+   */
   addRow(key: string, value: string): Table {
     this.rows.push({ key, value })
     return this
   }
 
+  /**
+   * Returns the table as a string.
+   *
+   * ```javascript
+   * console.log(table.toString())
+   * // output:
+   * // ┌───────────┬─────────────┐
+   * // │Key title  │Value title  │
+   * // ├───────────┼─────────────┤
+   * // │First key  │First value  │
+   * // ├───────────┼─────────────┤
+   * // │Second key │Second value │
+   * // └───────────┴─────────────┘
+   * ```
+   */
   toString(): string {
     let output: string = ''
 
